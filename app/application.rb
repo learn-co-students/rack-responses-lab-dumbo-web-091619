7789446547cb39458fa3ea31727252fa8e60f09a
solomonp90 <solomonraulpena@gmail.com>
2019-10-05 23:15:14 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,18 +5,20 @@ class Application
         time_1 = Time.new(2015,11,27,9,30)
         time_2 = Time.new(2015,11,27,14,30)
         time_3 = Time.new(2015,11,27,11,59)
-        time_4 = Time.now
+        
         
 
-        resp.write "#{time_1}\n"
-        resp.write "#{time_2}\n"
+        
+        
 
         if time_1 == time_3 || time_1 < time_3
-            resp.write "Good Morning"
+            resp.write "#{time_1}\n"
+            resp.write "Good Morning\n"
         end
         
         if time_2 > time_3
-            resp.write "Good Afternoon"
+            resp.write "#{time_2}\n"
+            resp.write "Good Afternoon\n"
         end     
 
         resp.finish
